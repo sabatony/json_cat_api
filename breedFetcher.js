5683fafd906c9c9c76cb4093a4f96dad2806ddf8
Sabastian Omeasoo <sabomeasoo@gmail.com>
2022-08-03 18:22:32 -0700

    test done

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,14 @@
 const request = require('request');
-const args = process.argv[2];
-const fetchBreedDescription = function(args) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (err,res,body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err,res,body) => {
     if (err) {
-      console.error('request failed to get details');
+      callback('request failed to get details');
     } else {
       let data = JSON.parse(body);
-      if (data) {
-        console.log(data[0].description);
+      if (data.length > 0) {
+        callback(null, data[0].description);
       } else {
-        console.log('breed not found');
+        callback('breed not found');
       }
 
       
@@ -17,6 +16,7 @@ const fetchBreedDescription = function(args) {
     }
 
   });
-};
+}
 
-fetchBreedDescription(args);
+
+module.exports = { fetchBreedDescription };
